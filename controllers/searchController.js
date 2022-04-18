a2f248cb342e3d1ba014ae042971dc470a30ef78
stephanieesquire <stephanieesquiremarco@gmail.com>
2022-04-18 00:31:04 -0300

    add not results found & code 404

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -33,25 +33,10 @@ async function artistAlbums(req, res) {
 
     const dataArtists = await resultArtists.json();
 
-    //get artist albums
-    const resultArtistAlbumsPerPage = await fetch(
-      `${process.env.SPOTIFY_API_URL}/artists/${dataArtists.artists.items[0].id}/albums?limit=50&include_groups=album`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${dataToken.access_token}}`,
-        },
-      }
-    );
-
-    let dataArtistAlbumsPerPage = await resultArtistAlbumsPerPage.json();
-    const dataArtistAlbums = [];
-    dataArtistAlbums.push(...dataArtistAlbumsPerPage.items);
-
-    //api pager
-    while (dataArtistAlbumsPerPage.next !== null) {
+    if (dataArtists.artists.items.length > 0) {
+      //get artist albums
       const resultArtistAlbumsPerPage = await fetch(
-        `${dataArtistAlbumsPerPage.next}`,
+        `${process.env.SPOTIFY_API_URL}/artists/${dataArtists.artists.items[0].id}/albums?limit=50&include_groups=album`,
         {
           method: "GET",
           headers: {
@@ -59,24 +44,15 @@ async function artistAlbums(req, res) {
           },
         }
       );
-      dataArtistAlbumsPerPage = await resultArtistAlbumsPerPage.json();
+
+      let dataArtistAlbumsPerPage = await resultArtistAlbumsPerPage.json();
+      const dataArtistAlbums = [];
       dataArtistAlbums.push(...dataArtistAlbumsPerPage.items);
-    }
 
-    //get albums data
-    let albumsIds = "";
-    const albumsData = [];
-
-    for (let i = 0; i < dataArtistAlbums.length; i++) {
-      albumsIds += dataArtistAlbums[i].id + ",";
-
-      //get several albums (max 20 ids)
-      if ((i + 1) % 20 === 0) {
-        const resultAlbumsDetails = await fetch(
-          `${process.env.SPOTIFY_API_URL}/albums/?ids=${albumsIds.slice(
-            0,
-            -1
-          )}`,
+      //api pager
+      while (dataArtistAlbumsPerPage.next !== null) {
+        const resultArtistAlbumsPerPage = await fetch(
+          `${dataArtistAlbumsPerPage.next}`,
           {
             method: "GET",
             headers: {
@@ -84,42 +60,74 @@ async function artistAlbums(req, res) {
             },
           }
         );
-
-        const dataAlbumsDetails = await resultAlbumsDetails.json();
-
-        albumsData.push(...dataAlbumsDetails.albums);
-        albumsIds = "";
+        dataArtistAlbumsPerPage = await resultArtistAlbumsPerPage.json();
+        dataArtistAlbums.push(...dataArtistAlbumsPerPage.items);
       }
 
-      //last call
-      else if (
-        dataArtistAlbums.length -
-          albumsData.length -
-          albumsIds.slice(0, -1).split(",").length ===
-        0
-      ) {
-        const resultAlbumsDetails = await fetch(
-          `${process.env.SPOTIFY_API_URL}/albums/?ids=${albumsIds.slice(
-            0,
-            -1
-          )}`,
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${dataToken.access_token}}`,
-            },
+      if (dataArtistAlbums.length > 0) {
+        //get albums data
+        let albumsIds = "";
+        const albumsData = [];
+
+        for (let i = 0; i < dataArtistAlbums.length; i++) {
+          albumsIds += dataArtistAlbums[i].id + ",";
+
+          //get several albums (max 20 ids)
+          if ((i + 1) % 20 === 0) {
+            const resultAlbumsDetails = await fetch(
+              `${process.env.SPOTIFY_API_URL}/albums/?ids=${albumsIds.slice(
+                0,
+                -1
+              )}`,
+              {
+                method: "GET",
+                headers: {
+                  Authorization: `Bearer ${dataToken.access_token}}`,
+                },
+              }
+            );
+
+            const dataAlbumsDetails = await resultAlbumsDetails.json();
+
+            albumsData.push(...dataAlbumsDetails.albums);
+            albumsIds = "";
           }
-        );
-        const dataAlbumsDetails = await resultAlbumsDetails.json();
 
-        albumsData.push(...dataAlbumsDetails.albums);
-        albumsIds = "";
+          //last call
+          else if (
+            dataArtistAlbums.length -
+              albumsData.length -
+              albumsIds.slice(0, -1).split(",").length ===
+            0
+          ) {
+            const resultAlbumsDetails = await fetch(
+              `${process.env.SPOTIFY_API_URL}/albums/?ids=${albumsIds.slice(
+                0,
+                -1
+              )}`,
+              {
+                method: "GET",
+                headers: {
+                  Authorization: `Bearer ${dataToken.access_token}}`,
+                },
+              }
+            );
+            const dataAlbumsDetails = await resultAlbumsDetails.json();
+
+            albumsData.push(...dataAlbumsDetails.albums);
+            albumsIds = "";
+          }
+        }
+
+        res.json(albumsData.sort((a, b) => b.popularity - a.popularity));
+      } else {
+        res.status(404).json({ code: 404, message: "not results found" });
       }
+    } else {
+      res.status(404).json({ code: 404, message: "not results found" });
     }
-
-    res.json(albumsData.sort((a, b) => b.popularity - a.popularity));
   } catch (error) {
-    res.status(400).json({ message: "an error has ocurred" });
+    res.status(400).json({ code: 400, message: "an error has ocurred" });
   }
 }
 
