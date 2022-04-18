5e042d7caa1c0bf51e4440af502c8d285272e67f
stephanieesquire <stephanieesquiremarco@gmail.com>
2022-04-18 01:51:06 -0300

    create log (store db)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const db = require("./db");
 const APP_PORT = process.env.APP_PORT;
 const app = express();
 
@@ -10,7 +11,8 @@ const app = express();
 app.use(cors());
 
 routes(app);
+db();
 
-app.listen(APP_PORT, () =>
+app.listen(APP_PORT, "0.0.0.0", () =>
   console.log(`[Express] Server running on port ${APP_PORT}`)
 );
